a8a1ae29193a9344ca0b0a55bc04adc14a18c431
d3h1 <denicabaravdic@gmail.com>
2023-11-28 14:41:26 -0800

    one more time with the api call back

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-// const API_URL = import.meta.env.VITE_RAWG_URL;
-// const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
+const API_URL = import.meta.env.VITE_RAWG_URL;
+const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
 
 
 export default axios.create({
-	baseURL: "https://api.rawg.io/api/",
+	baseURL: API_URL,
 	params: {
-		key: "d67e0f8e51ad4ca1b9810a54dbbaa778",
+		key: API_KEY,
 	},
 	withCredentials: false,
 });
