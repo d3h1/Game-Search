80eedd56be179b64e747e2a0b3420848191790ab
d3h1 <denicabaravdic@gmail.com>
2023-12-11 08:51:06 -0800

    fixing filtering

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,23 +3,23 @@ import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
 interface Props {
-  onSearch: (searchText: string) => void;
+	onSearch: (searchText: string) => void;
 }
 
-const SearchInput = ({onSearch} : Props) => {
+const SearchInput = ({ onSearch }: Props) => {
 	const ref = useRef<HTMLInputElement>(null);
 
 	return (
-    <form
+		<form
 			onSubmit={(event) => {
-        event.preventDefault();
-        if(ref.current) onSearch(ref.current.value)
+				event.preventDefault();
+				if (ref.current) onSearch(ref.current.value);
 			}}
 		>
 			<InputGroup>
 				<InputLeftElement children={<BsSearch />} />
-        <Input
-          ref={ref}
+				<Input
+					ref={ref}
 					borderRadius={20}
 					placeholder="Search games . . ."
 					variant={"filled"}
